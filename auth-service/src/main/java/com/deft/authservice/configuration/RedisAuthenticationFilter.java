246e9afdce19f5fd04a34e980c1284134fc4fcd9
deft <deft1991@gmail.com>
2023-10-05 21:30:14 +0600

    Add admin role

---
+++ auth-service/src/main/java/com/deft/authservice/configuration/RedisAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.deft.authservice.configuration;
+
+import com.deft.authservice.service.impl.AuthServiceRedis;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+/**
+ * @author Sergey Golitsyn
+ * created on 05.10.2023
+ */
+
+@Component
+@RequiredArgsConstructor
+public class RedisAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthServiceRedis authServiceRedis;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        Optional<Authentication> authentication = this.authServiceRedis.authenticate(request);
+        authentication.ifPresent(SecurityContextHolder.getContext()::setAuthentication);
+        filterChain.doFilter(request, response);
+    }
+}
